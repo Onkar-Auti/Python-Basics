a689f2b296c58dd2e8bf25773f8b969b4bfe96eb
Onkar Auti <onkarauti1@gmail.com>
2025-08-21 14:21:12 +0530

    Assignment 1 Completed

---
+++ Learning.py
@@ -0,0 +1,22 @@
+# Strings
+print('Python\'s "World"')
+
+print("Item \tQty \nApple \t5 \nplums \t10")
+
+print("I am a polite person.\n" * 3)
+
+# Variables
+var1 = "Hello World"
+print(var1)
+
+# Simple Intrest Calculator
+P = input("Enter P : ")
+N = input("Enter N : ")
+R = input("Enter R : ")
+P = int(P)
+N = int(N)
+R = int(R)
+
+SI = (P*N*R)/100
+
+print("Intrest is ",SI)
